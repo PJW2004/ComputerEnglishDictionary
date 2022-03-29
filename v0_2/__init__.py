3837fe2d19666a715505ac2a582a72985ed5fbb1
machine <appletreegood123@naver.com>
2022-03-29 19:52:39 +0900

    wark

--- v0_2/__init__.py
+++ v0_2/__init__.py
@@ -1,18 +1,13 @@
-import API.in_word as InWord
-import API.delete_word as DeleteWord
-import API.learning_word as LearningWord
-import API.end as End
-
-print('''
-########################################
-                환영 합니다!    
-########################################''')
+import os
 
 User_answers = {
-    '용어 입력': InWord,
-    '용어 삭제': DeleteWord,
-    '단어 학습': LearningWord,
-    '종료': End
+    '용어 입력': 'python ./API/in_word.py',
+    '용어 삭제': 'python ./API/delete_word.py',
+    '단어 학습': 'python ./API/learning_word.py',
+    '종료': 'python ./API/end.py'
 }
 
-
+os.system(User_answers[input('''
+    [용어 입력, 용어 삭제, 단어 학습, 종료]
+👉 위에 있는 데로 똑같이 작성을 해 주셔야 합니다. 👈
+''')])
