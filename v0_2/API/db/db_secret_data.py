5bb936670595d460dcf4ebb101e4cde241f1cb52
machine <appletreegood123@naver.com>
2022-03-31 22:52:53 +0900

    # correction

--- v0_2/API/db/db_secret_data.py
+++ v0_2/API/db/db_secret_data.py
@@ -5,6 +5,6 @@ load_dotenv()
 
 DB_user = os.environ.get('DB_USER')
 DB_passwd = os.environ.get('DB_PASSWD')
-DB_IP = os.environ.get('DB_IP')
+DB_host = os.environ.get('DB_IP')
 DB_port = os.environ.get('DB_PORT')
 DB_name = os.environ.get('DBNAME')
