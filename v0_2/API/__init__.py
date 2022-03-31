d93bf1ffa977819939a7c511d9a9990344e5ce93
machine <appletreegood123@naver.com>
2022-03-31 22:17:46 +0900

    # api correction

--- v0_2/API/__init__.py
+++ v0_2/API/__init__.py
@@ -1,9 +1,2 @@
-import os
-from dotenv import load_dotenv
+# API Start
 
-load_dotenv()
-
-DB_user = os.environ.get('DB_USER')
-DB_passwd = os.environ.get('DB_PASSWD')
-print(DB_user)
-print(DB_passwd)
