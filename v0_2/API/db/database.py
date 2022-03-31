a97ca02c55252eba3a2a404ed23b8db2356e56b7
machine <appletreegood123@naver.com>
2022-03-31 23:07:59 +0900

    # db set

--- v0_2/API/db/database.py
+++ v0_2/API/db/database.py
@@ -4,7 +4,12 @@ from db_secret_data import DB_host
 from db_secret_data import DB_port
 from db_secret_data import DB_name
 
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
 url = f'postgresql://{DB_user}:{DB_passwd}@{DB_host}:{DB_port}/{DB_name}'
-
-
+engine = create_engine(url)
+Session = sessionmaker(bind=engine)
+Base = declarative_base
 
