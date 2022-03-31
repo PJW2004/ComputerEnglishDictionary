09e75d88274338ce74052bcf835b484e775156b1
machine <appletreegood123@naver.com>
2022-03-31 22:55:09 +0900

    # db test

--- v0_2/API/db/database.py
+++ v0_2/API/db/database.py
@@ -1,4 +1,8 @@
 from db_secret_data import DB_user
 from db_secret_data import DB_passwd
-from db_secret_data import DB_IP
+from db_secret_data import DB_host
+from db_secret_data import DB_port
+from db_secret_data import DB_name
+
+
 
