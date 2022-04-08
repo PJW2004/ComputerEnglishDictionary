881af95a6b1d8f9e150c249ef191d54990c067fd
machine <appletreegood123@naver.com>
2022-04-08 23:52:03 +0900

    test

--- v0_2/API/db/database.py
+++ v0_2/API/db/database.py
@@ -5,13 +5,8 @@ from db_secret_data import DB_port
 from db_secret_data import DB_name
 print('env setting')
 
-# url = f'postgresql://{DB_user}:{DB_passwd}@{DB_host}:{DB_port}/{DB_name}'
+
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
 
-url = f"postgresql://postgres:postgres@localhost:5432/postgres"
+url = f'postgresql://{DB_user}:{DB_passwd}@{DB_host}:{DB_port}/{DB_name}'
 engine = create_engine(url)
-SessionLocal = sessionmaker(bind=engine)
-
-Base = declarative_base()
