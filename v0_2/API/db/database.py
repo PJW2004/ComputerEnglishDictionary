e3b104cfc0185969cf816a46e2eccfd272f42607
machine <appletreegood123@naver.com>
2022-04-09 00:12:12 +0900

    # solution

--- v0_2/API/db/database.py
+++ v0_2/API/db/database.py
@@ -3,10 +3,10 @@ from db_secret_data import DB_passwd
 from db_secret_data import DB_host
 from db_secret_data import DB_port
 from db_secret_data import DB_name
-print('env setting')
-
 
 from sqlalchemy import create_engine
 
+# psycopg 에러와 같은 경우 rosetta ERROR 때문에 사용 못함
+# -> window에서 사용가능
 url = f'postgresql://{DB_user}:{DB_passwd}@{DB_host}:{DB_port}/{DB_name}'
 engine = create_engine(url)
