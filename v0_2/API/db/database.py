e123e91225609fc24fd6ce7e7ca06b5a1fc49049
MACHINE\jwdev <appletreegood123@naver.com>
2022-04-09 09:47:21 +0900

    # error resolution

---
+++ v0_2/API/db/database.py
@@ -0,0 +1,17 @@
+from API.db.db_secret_data import DB_user
+from API.db.db_secret_data import DB_passwd
+from API.db.db_secret_data import DB_host
+from API.db.db_secret_data import DB_port
+from API.db.db_secret_data import DB_name
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+# psycopg 에러와 같은 경우 rosetta ERROR 때문에 사용 못함
+# -> window에서 사용가능
+url = f'postgresql://{DB_user}:{DB_passwd}@{DB_host}:{DB_port}/{DB_name}'
+engine = create_engine(url)
+SessionLocal = sessionmaker(engine)
+
+Base = declarative_base
