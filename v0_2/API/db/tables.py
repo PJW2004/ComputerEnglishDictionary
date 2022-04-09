8146673a9f604e6b125788b7b34310e298ce3c92
MACHINE\jwdev <appletreegood123@naver.com>
2022-04-09 09:16:24 +0900

    # attempted relative import with no known parent package

--- v0_2/API/db/tables.py
+++ v0_2/API/db/tables.py
@@ -6,11 +6,11 @@ from sqlalchemy import MetaData
 # database engine setting
 try:
     from .database import engine
-except ImportError:
-    os.system('pip3 uninstall psycopg2')
-    os.system('pip3 install psycopg2-binary')
-    # postgres APP 사용 경우
-    # os.system('sudo ln -s /Applications/Postgres.app/Contents/Versions/9.4/lib/libpq.5.dylib /usr/lib')
+# except ImportError:
+#     os.system('pip3 uninstall psycopg2')
+#     os.system('pip3 install psycopg2-binary')
+#     # postgres APP 사용 경우
+#     # os.system('sudo ln -s /Applications/Postgres.app/Contents/Versions/9.4/lib/libpq.5.dylib /usr/lib')
 finally:
     print('module change')
 
@@ -23,21 +23,22 @@ create table list
 # metadata
 meta = MetaData()
 
-userdata = Table(
-    'userdata', meta,
-    Column('word', String, primary_key=True),
-    Column('meaning', String, unique=False),
-    # foreign Key
-    Column('user', String, ForeignKey("user_list.user"), unique=False)
-)
+if __name__ == "__main__":
+    userdata = Table(
+        'userdata', meta,
+        Column('word', String, primary_key=True),
+        Column('meaning', String, unique=False),
+        # foreign Key
+        Column('user', String, ForeignKey("user_list.user"), unique=False)
+    )
 
-User_list = Table(
-    'user_list', meta,
-    Column('user', String, primary_key=True),
-    Column('passwd', String, unique=False)
-)
+    User_list = Table(
+        'user_list', meta,
+        Column('user', String, primary_key=True),
+        Column('passwd', String, unique=False)
+    )
 
-# Table create
-with engine.connect as conn:
-    meta.create_all(conn, checkfirst=False)
+    # Table create
+    with engine.connect as conn:
+        meta.create_all(conn, checkfirst=False)
 
