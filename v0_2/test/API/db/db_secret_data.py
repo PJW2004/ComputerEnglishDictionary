7b4b381890cde31e241b2824a7e21b6b4b7a5004
MACHINE\jwdev <appletreegood123@naver.com>
2022-04-10 12:11:53 +0900

    # [edit]

---
+++ v0_2/test/API/db/db_secret_data.py
@@ -0,0 +1,10 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DB_user = os.environ.get('TEST_DB')
+DB_passwd = os.environ.get('TEST_PASSWD')
+DB_host = os.environ.get('TEST_IP')
+DB_port = os.environ.get('TEST_PORT')
+DB_name = os.environ.get('TEST_DBNAME')
