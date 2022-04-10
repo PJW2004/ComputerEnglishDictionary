fc8e09ff29b747d1b67f41de9e2aefc6f4496ec7
MACHINE\jwdev <appletreegood123@naver.com>
2022-04-10 12:15:21 +0900

    # interim storage

--- v0_2/test/API/db/database.py
+++ v0_2/test/API/db/database.py
@@ -8,3 +8,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
+url = f"postgresql://{DB_user}:{DB_passwd}@{DB_host}:{DB_port}/{DB_name}"
+engine = create_engine(url)
+SessionLocal = sessionmaker(engine)
+
+Base = declarative_base
