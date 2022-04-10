b0e9058c9431127621590ad6cae580a3f8158fa2
MACHINE\jwdev <appletreegood123@naver.com>
2022-04-10 12:10:32 +0900

    # setting

--- v0_2/test/API/db/database.py
+++ v0_2/test/API/db/database.py
@@ -3,3 +3,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+DB_user = os.environ.get('TEST_DB')
+DB_passwd = os.environ.get('TEST_PASSWD')
+DB_host = os.environ.get('TEST_IP')
+DB_port = os.environ.get('TEST_PORT')
+DB_name = os.environ.get('TEST_DBNAME')
