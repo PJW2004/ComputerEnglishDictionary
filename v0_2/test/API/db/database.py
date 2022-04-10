00d6dabc0d38b964da43560867f8a34bdf43656d
MACHINE\jwdev <appletreegood123@naver.com>
2022-04-10 12:12:39 +0900

    # module

---
+++ v0_2/test/API/db/database.py
@@ -0,0 +1,10 @@
+from API.db.db_secret_data import DB_user
+from API.db.db_secret_data import DB_passwd
+from API.db.db_secret_data import DB_host
+from API.db.db_secret_data import DB_port
+from API.db.db_secret_data import DB_name
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
