8584c7e5c568f55c09d758e1e9041df857ec36ec
MACHINE\jwdev <appletreegood123@naver.com>
2022-04-14 16:13:38 +0900

    # user table interim storage

--- v0_2/API/db/models.py
+++ v0_2/API/db/models.py
@@ -1,10 +1,11 @@
 from .database import Base
 from sqlalchemy import Column, String, ForeignKey
 
-class userdata(Base):
-    __tablename__ == 'userdata'
 
-    Column('word', String, primary_key=True),
-    Column('meaning', String, unique=False),
+class userdata(Base):
+    __tablename__ = 'userdata'
+
+    word = Column('word', String, primary_key=True)
+    meaning = Column('meaning', String, unique=False)
     # foreign Key
-    Column('user', String, ForeignKey("user_list.user"), unique=False)
+    user = Column('user', String, ForeignKey("user_list.user"), unique=False)
