a24e96b647aa537806e87298d48081e9971d8014
MACHINE\jwdev <appletreegood123@naver.com>
2022-04-14 16:01:01 +0900

    # interim storage

--- v0_2/test/API/db/db_secret_data.py
+++ v0_2/test/API/db/db_secret_data.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DB_user = os.environ.get('TEST_DB')
+DB_user = os.environ.get('TEST_USER')
 DB_passwd = os.environ.get('TEST_PASSWD')
 DB_host = os.environ.get('TEST_IP')
 DB_port = os.environ.get('TEST_PORT')
