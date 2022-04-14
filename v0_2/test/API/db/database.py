4393a5c3c61b86bcb37be241ca1aab60fa0ed42a
MACHINE\jwdev <appletreegood123@naver.com>
2022-04-14 16:01:15 +0900

    # interim storage database

--- v0_2/test/API/db/database.py
+++ v0_2/test/API/db/database.py
@@ -1,8 +1,8 @@
 from API.db.db_secret_data import DB_user
-from API.db.db_secret_data import DB_passwd
 from API.db.db_secret_data import DB_host
 from API.db.db_secret_data import DB_port
 from API.db.db_secret_data import DB_name
+from API.db.db_secret_data import DB_passwd
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
