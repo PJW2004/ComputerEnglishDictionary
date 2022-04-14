1a06ca2951757c9366fa63d0dfd4acbd9d6c8910
MACHINE\jwdev <appletreegood123@naver.com>
2022-04-15 00:23:50 +0900

    # interim storage

--- v0_2/API/db/db_secret_data.py
+++ v0_2/API/db/db_secret_data.py
@@ -3,8 +3,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DB_user = os.environ.get('DB_USER')
-DB_passwd = os.environ.get('DB_PASSWD')
+# DB_user = os.environ.get('DB_USER')
+# DB_passwd = os.environ.get('DB_PASSWD')
+# DB_host = os.environ.get('DB_IP')
+# DB_port = os.environ.get('DB_PORT')
+# DB_name = os.environ.get('DBNAME')
+
+DB_user = os.environ.get('TEST_USER')
+DB_passwd = os.environ.get('TEST_PASSWD')
 DB_host = os.environ.get('DB_IP')
-DB_port = os.environ.get('DB_PORT')
-DB_name = os.environ.get('DBNAME')
+DB_port = os.environ.get('TEST_PORT')
+DB_name = os.environ.get('TEST_DBNAME')
