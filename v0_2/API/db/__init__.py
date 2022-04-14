608b392a95eab91f8db017fd3f1ece6f25113476
MACHINE\jwdev <appletreegood123@naver.com>
2022-04-14 16:22:26 +0900

    # interim

--- v0_2/API/db/__init__.py
+++ v0_2/API/db/__init__.py
@@ -1,8 +1,7 @@
-if __name__ == "__main__": # absolute path
-    from API.db.database import engine
+from API.db.database import engine
 
+if __name__ == "__main__": # absolute path
     # Table create 아니 포트를 바꾸어도 이렇게 안되네 씁
     conn = engine.connect()
-    # with engine.connect as conn:
-    #     meta.create_all(conn, checkfirst=False)
+    
 
