926664c1f8fc8a1476b7a5cf6fa5d9c3387a4a4a
MACHINE\jwdev <appletreegood123@naver.com>
2022-04-15 23:08:01 +0900

    # [EDIT] write to Problem solving methods

--- v0_2/API/db/__init__.py
+++ v0_2/API/db/__init__.py
@@ -3,6 +3,7 @@ import models
 
 if __name__ == "__main__": # absolute path
     # Table create 아니 포트를 바꾸어도 이렇게 안되네 씁
+    # => 연결이 안된이유 : aws서버에서 inbound port를 안열어줌 ㅇㅇ
     conn = engine.connect()
 
 
