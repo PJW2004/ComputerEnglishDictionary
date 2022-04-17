8a6ade6d3762c5479cc3bf76d03989e2b42df3a6
MACHINE\jwdev <appletreegood123@naver.com>
2022-04-17 23:12:28 +0900

    # data in

--- v0_2/API/in_word.py
+++ v0_2/API/in_word.py
@@ -1,6 +1,5 @@
 from translate import get_translate
 user = input('[입력할 용어를 적어 주세요]\n[한 👉 영] : ')
 
-data = get_translate(user)
-
-
+data = {user: get_translate(user)}
+YN = (f'[data가 최종적으로 이렇게 저장이 됩니다.]\n[변경하지 않겠습니까? (Y/N)]')
