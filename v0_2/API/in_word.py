5d90740a6c8204fe357f54b4ebf3e7c565275218
MACHINE\jwdev <appletreegood123@naver.com>
2022-04-17 23:15:54 +0900

    # interim storage

--- v0_2/API/in_word.py
+++ v0_2/API/in_word.py
@@ -2,11 +2,10 @@ from translate import get_translate
 user = input('[입력할 용어를 적어 주세요]\n[한 👉 영] : ')
 
 data = {user: get_translate(user)}
-YN = (f'[data가 최종적으로 이렇게 저장이 됩니다.]\n[변경하지 않겠습니까? (Y/N)] : ')
+YN = input(f'[data가 최종적으로 이렇게 저장이 됩니다.]\n[변경하지 않겠습니까? (Y/N)] : ')
 
 if YN == 'Y':
     data = {user: input('[변경될 영어 번역을 입력해 주세요] : ')}
     print(data)
 else:
     print(data)
-    
