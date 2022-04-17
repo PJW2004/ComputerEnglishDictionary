e456f43e8ee8aad2b5d21dcf8549f9bde8929070
MACHINE\jwdev <appletreegood123@naver.com>
2022-04-17 11:37:36 +0900

    # interim storage

--- v0_2/API/translate.py
+++ v0_2/API/translate.py
@@ -1,5 +1,6 @@
 import requests
 
+
 def get_translate(text):
     client_id, client_secret = 'LnbkGpZ1FltpJ_Jytof0', '2odQqgaDim'
     url = 'https://openapi.naver.com/v1/papago/n2mt'
@@ -11,3 +12,8 @@ def get_translate(text):
         "X-Naver-Client-Id": client_id,
         "X-Naver-Client-Secret": client_secret
             }
+
+    response = requests.post(url, headers=header, data=data)
+    send_data = response.json()
+    print(response)
+    print(send_data)
