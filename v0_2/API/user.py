477ec50e3b15cc0a233fb0e2a91d9ae2e9ee1f0b
MACHINE\jwdev <appletreegood123@naver.com>
2022-04-18 00:53:16 +0900

    # [EDIT] user

--- v0_2/API/user.py
+++ v0_2/API/user.py
@@ -7,6 +7,8 @@ from API.db import models
 Base.metadata.create_all(engine)
 
 print('[로그인을 먼저 해주셔야 프로그램이 시작됩니다.] : 계정이 없다면 "회원가입"을 입력해주세요')
+
+def
 user = input('user : ')
 conn = engine.connect()
 
@@ -15,16 +17,24 @@ def create_user():
     user = input('[회원가입할 username을 입력해주세요][영어만 지원이 됩니다.] : ')
     passwd = input('[user의 비번은?] : ')
     insert = f"INSERT INTO userlist VALUES('{user}', '{passwd}')"
-    print(insert)
+
     conn.execute(insert)
 
 
 if user == '회원가입':
     try:
         create_user()
+
+        print("[정상적으로 만들어 졌습니다.]\n")
+
+
     except sqlalchemy.exc.IntegrityError:
-        print('[이미 존재하는 계정 이름입니다.]')
+        print('[이미 존재하는 계정 이름입니다.]\n')
         create_user()
 
 else:
-    pass
+    ins = "select users_ from userlist"
+
+    result = conn.execute(ins)
+    for i in result:
+        print(i)
