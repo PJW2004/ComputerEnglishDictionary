fae849b75173378990d79b76a0c78deed60ec6f4
MACHINE\jwdev <appletreegood123@naver.com>
2022-04-18 01:28:25 +0900

    # interim storage

--- v0_2/API/user.py
+++ v0_2/API/user.py
@@ -28,33 +28,44 @@ def running_cre_u():
     try:
         create_user()
 
-        print("[정상적으로 만들어 졌습니다.]\n")
+        print("[정상적으로 만들어 졌습니다.]\n[프로그램이 안전하게 돌아가기 위해 다시 시작해주세요.]")
 
     except sqlalchemy.exc.IntegrityError:
         print('[이미 존재하는 계정 이름입니다.]\n')
         create_user()
 
-        
+
 def running(user_="", user_li=None):
     if user_ == '회원가입':
-
+        running_cre_u()
     elif user_ in user_li:
-        print(user_)
+        passwd = input('passwd : ')
+        if passwd == passwd_li[user_li.index(user_)]:
+            print('[정상적으로 로그인이 되었습니다.]')
+            return True
+        else:
+            print('[로그인 실패]')
+            return False
+
     else:
         YN = input('[계정이 존재 하지 않습니다. 계정을 만드시겠습니까?][Y/N] : ')
+        if YN == 'Y':
+            running_cre_u()
+        else:
+            print('프로그램을 종료합니다.')
+            return False
 
 
-ins = "select users_ from userlist"
+ins = "select * from userlist"
 
 result = conn.execute(ins)
 user_li = []
+passwd_li = []
 for i in result:
     user_li.append(i[0])
+    passwd_li.append(i[1])
 
-print(user_li)
+print(user_li, passwd_li)
 
 user = user_login()
-
-running(user_=user, user_li=user_li)
-
-
+TF = running(user_=user, user_li=user_li)
