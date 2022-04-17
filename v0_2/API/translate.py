39c40f88f55cd66c84aa4db044eb32b2dcf03881
MACHINE\jwdev <appletreegood123@naver.com>
2022-04-17 11:33:12 +0900

    # data

--- v0_2/API/translate.py
+++ v0_2/API/translate.py
@@ -1,5 +1,9 @@
-import reqests
+import requests
 
 def get_translate(text):
     client_id, client_secret = 'LnbkGpZ1FltpJ_Jytof0', '2odQqgaDim'
     url = 'https://openapi.naver.com/v1/papago/n2mt'
+    data = {'text': text,
+            'source': 'ko',
+            'target': 'en'}
+        
