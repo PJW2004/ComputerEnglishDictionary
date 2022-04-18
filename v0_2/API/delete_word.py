0102081b66695b199b0d318bc81f44d45b2c202c
MACHINE\jwdev <appletreegood123@naver.com>
2022-04-18 22:19:58 +0900

    # [EDIT] word

--- v0_2/API/delete_word.py
+++ v0_2/API/delete_word.py
@@ -5,6 +5,8 @@ conn = engine.coonnect()
 
 user = input('[삭제할 대상을 입력해 주세요][user/word] : ')
 if user == 'user':
-    data = input("[삭제할 계정 이름을 입력해 주세요.]")
-    delete = f'DELETE FROM userlist WHERE users_="{data}"'
-    
+    user_data = input("[삭제할 계정 이름을 입력해 주세요.] : ")
+    passwd = input("[passwd] : ")
+    delete = f'DELETE FROM userlist WHERE users_="{user_data}"'
+
+    conn.execute(delete)
