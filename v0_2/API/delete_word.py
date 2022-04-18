0b11aa546ab9d9a7932a06926b805edcc2b4c368
MACHINE\jwdev <appletreegood123@naver.com>
2022-04-18 22:23:54 +0900

    # interim storage

--- v0_2/API/delete_word.py
+++ v0_2/API/delete_word.py
@@ -1,10 +1,11 @@
-from .db.database import engine
+from API.db.database import engine
 
 conn = engine.coonnect()
 
 
 user = input('[삭제할 대상을 입력해 주세요][user/word] : ')
 if user == 'user':
+    print(user_li, passwd_li)
     user_data = input("[삭제할 계정 이름을 입력해 주세요.] : ")
     passwd = input("[passwd] : ")
     delete = f'DELETE FROM userlist WHERE users_="{user_data}"'
